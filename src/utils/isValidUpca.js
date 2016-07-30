506d1b370e100c4654d19da4ecd86383d7812e64
Vincent TAING <vnctaing@vnctaingggg.local>
2016-07-30 15:18:11 +0200

    write tests for isValidUpca() and refactor isValidUpca

--- src/utils/isValidUpca.js
+++ src/utils/isValidUpca.js
@@ -11,23 +11,23 @@ export default function isValidUpca(code) {
   if (isNaN(code)) {
     reasons.push("UPC-A contains only numbers")
   } else {
-    if (!isTwelveCharacterLong) {
-      reasons.push("It isn't a valid UPC-A code (12 characters)")
-    }
-    
+
     if (isElevenCharacterLong) {
       const additionalHint = isCheckDigitValid(code) 
         ? 'Leading zero might have been deleted'
         : 'The check digit is wrong';
       reasons.push(additionalHint);
     }
-
-    if(!isCheckDigitValid(code)) {
-      reasons.push('Unvalid UPC-A code. Check for mispell.')
+    else if (!isTwelveCharacterLong) {
+      reasons.push("It isn't a valid UPC-A code (12 characters)")
     }
+    else {
+      reasons.push('Unvalid UPC-A code. Check for mispell')
+    }
+
   }
+
   
   const isValid = !isNaN(code) && isTwelveCharacterLong && isCheckDigitValid(code);
-
   return isValid || reasons;
 }
--- test/test.js
+++ test/test.js
@@ -1,18 +1,35 @@
 const assert = require('chai').assert;
 const isValidUpca = require('../src/utils/isValidUpca');
 
+describe('Utils function', function() {
+  describe('isValidUpca()', function() {
+    it('should return return when the value a valid upc-a', function() {
+      assert.equal(true, isValidUpca.default('889714000045'));
+    });
 
+    it("should return ['UPC-A contains only numbers'] when the value is not a number", function() {
+      assert.deepEqual(['UPC-A contains only numbers'], isValidUpca.default('8abde') )
+    });
 
-describe('Array', function() {
-  describe('#indexOf()', function() {
+    it("should return ['It isn't a valid UPC-A code (12 characters)'] when value is not a number of 12 digits", function() {
+      assert.deepEqual(["It isn't a valid UPC-A code (12 characters)"], isValidUpca.default('88971400004432123'))
+    });
+    
+    it("should.return ['Leading zero might have been deleted'] when UPC-A code miss the leading 0", function() {
+      assert.deepEqual(['Leading zero might have been deleted'], isValidUpca.default('82184090466'))
+    });
+
+    it("should.return['Unvalid UPC-A code. Check for mispell'] when 12 digit giberrish are submitted", function() {
+      assert.deepEqual(['Unvalid UPC-A code. Check for mispell'], isValidUpca.default('123123123123'))
+    });
+  });
+
+  describe('removeEveryWhiteSpace()', function() {
     it('should return -1 when the value is not present', function() {
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
     });
-    it('should return -1 when the value is not present bis', function() {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
-
   });
+
+
 });
